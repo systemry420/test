526ca46ae85f8f0eecfff65fcdcbc23f73edf0f1
Kassem <systemry420@gmail.com>
2022-05-20 21:24:07 +0300

    fix

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,6 @@
 import { DataService } from './../../service/data.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable, map, tap, BehaviorSubject } from 'rxjs';
+import { Observable, map, tap, BehaviorSubject, Subscription } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import * as _ from 'underscore'
 
@@ -12,20 +12,27 @@ import * as _ from 'underscore'
 export class TableComponent implements OnInit {
 
   users: User[] = []
+  subscription: Subscription
+
 
   constructor(
     private service: DataService
-  ) { }
-
-  ngOnInit(): void {
-    this.service.getData()
+  ) {
+    this.subscription = this.service.getData()
       .subscribe(users => {
         this.users = users
       })
+   }
+
+  ngOnInit(): void {
   }
 
   sort(key: string) {
     this.users = _.sortBy(this.users, key)
+  }
+
+  ngDestroy() {
+    this.subscription.unsubscribe()
   }
 
 }
